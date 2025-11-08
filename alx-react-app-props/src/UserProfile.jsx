bfd6784729f8c04cdcece48d91530441e31597c4
Walin <89344253+Mrcutthr0at@users.noreply.github.com>
2025-11-08 13:48:41 +0300

    hh

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -5,9 +5,9 @@ function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
-    <div style={{ border: '1px solid #ccc', padding: '20px', margin: '20px', borderRadius: '8px' }}>
-      <p><strong>Name:</strong> {userData.name}</p>
-      <p><strong>Email:</strong> {userData.email}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
