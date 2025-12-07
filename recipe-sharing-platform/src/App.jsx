332ad550e6e361ad25b46a6d0470bd60dfc640e7
Walin <89344253+Mrcutthr0at@users.noreply.github.com>
2025-12-07 18:05:09 +0300

    66

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,7 @@
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <h1 className="text-blue-500 text-3xl font-bold">
-      Recipe Sharing Platform Setup Complete!
-    </h1>
-  );
+  return <HomePage />;
 }
+
 export default App;
