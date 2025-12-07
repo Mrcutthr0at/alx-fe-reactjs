7d7b4fe3658390ab091acd416ab32b85dd47a38c
Walin <89344253+Mrcutthr0at@users.noreply.github.com>
2025-12-07 16:39:34 +0300

    pp

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  return (
-    <h1 className="text-4xl font-bold text-blue-600">
-      Tailwind is working!
-    </h1>
-  )
+  return <UserProfile />;
 }
-export default App
+
+export default App;
