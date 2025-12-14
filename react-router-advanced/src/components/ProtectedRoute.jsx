9603f95af76bba46e331732ec23dc5d7e5b848f4
Walin <89344253+Mrcutthr0at@users.noreply.github.com>
2025-12-14 20:04:33 +0300

    68

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate } from 'react-router-dom'
 
+const useAuth = () => {
+  return localStorage.getItem('auth') === 'true'
+}
+
 export default function ProtectedRoute({ children }) {
-  const isAuthenticated = localStorage.getItem('auth') === 'true'
+  const isAuthenticated = useAuth()
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />
